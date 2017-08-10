09937daeb7696fd46631fc444baf8346de9e27c9
maxbausher <maxbausher@gmail.com>
2017-08-10 16:51:48 +1000

    updated integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: ["./src/index.js"],
   output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+          presets: ["react", "es2015", "stage-1"]
+        }
       }
-    }]
+    ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ["", ".js", ".jsx"]
   },
-  devtool: '#inline-source-maps',
+  devtool: "#inline-source-maps",
   devServer: {
     historyApiFallback: true,
-    contentBase: './'
+    contentBase: "./"
   }
 };
